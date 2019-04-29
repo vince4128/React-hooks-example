62bd39a2b058ea279333250e8d23a9af0a06a085
vince4128 <vrlt.vincent@gmail.com>
2019-04-29 14:31:50 +0200

    convert class based component to functional component using hooks

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-class ResourceList extends Component{
+/*class ResourceList extends Component{
 
     state = {resources: []}
 
@@ -22,6 +22,32 @@ class ResourceList extends Component{
     render() {
         return <div>{this.state.resources.length}</div>;
     }
+
+}*/
+
+/* refactor the class component in functional component using hooks */
+
+const ResourceList = ({resource}) => {
+
+    const [resources, setResources] = useState([]);
+
+     const fetchResource = async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+
+        setResources(response.data);    
+    }
+
+    useEffect(() => {
+        fetchResource(resource)
+    }, [resource]);
+    /*
+        useEffect combine the componentDidUpdate and the componentDidMount method
+        we have to put, resource in the 2nd argument (array) for componenDidUpdate
+        
+        if there is no 2nd argument, the function is called every time (! warning could cause spam request, because fetchResource is called and call setResources who update the component and run useEffect again and again...)
+    */
+
+    return <div>{resources.length}</div>;
 
 }
 
